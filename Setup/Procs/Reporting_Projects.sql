bd37e7933544f2943e45698d7cb36a6c2e4e6311
Lee, Brian <brian.lee2@spglobal.com>
2025-05-20 09:00:10 -0500

    updated proc

--- Setup/Procs/Reporting_Projects.sql
+++ Setup/Procs/Reporting_Projects.sql
@@ -4,6 +4,7 @@ BEGIN
 	
 	DROP TABLE IF EXISTS #Remaining
 	DROP TABLE IF EXISTS #FinalResults		
+	DROP TABLE IF EXISTS #Projects2
 
 	DECLARE @DaysPerWeek decimal(4,2) = 4
 	DECLARE @Buffer decimal(4,2) = 1.15
@@ -13,18 +14,22 @@ BEGIN
 	FROM #Projects p
 	JOIN WorkItemTags wt on wt.Tag = p.Tag
 	JOIN WorkItems w on w.WorkItemId = wt.WorkItemId
-	WHERE w.Activity = p.Activity
+	WHERE EXISTS (SELECT value FROM STRING_SPLIT(p.Activity, '|') WHERE value = w.Activity)
 	GROUP BY p.Tag
 
+	SELECT *, CASE 
+	WHEN StartDate > GetDate() THEN StartDate ELSE GetDate() end as ActualStart
+	INTO #projects2
+	FROM #projects
 
 	select p.Tag, 
 	p.TotalCapacity,
 	p.DevDedication,
 	FORMAT(r.Remaining, 'N1') as Remaining, 
 	FORMAT(p.StartDate, 'MMMM dd yyyy') as StartDate, 
-	FORMAT(DATEADD(d, @Buffer*((r.Remaining/(p.TotalCapacity*p.DevDedication))/@DaysPerWeek*7) + p.QADays, p.StartDate), 'MMMM dd yyyy') as TargetDate
+	FORMAT(DATEADD(d, @Buffer*((r.Remaining/(p.TotalCapacity*p.DevDedication))/@DaysPerWeek*7) + p.QADays, p.ActualStart), 'MMMM dd yyyy') as TargetDate
 	INTO #FinalResults
-	FROM #Projects p
+	FROM #Projects2 p
 	JOIN #Remaining r on r.Tag = p.Tag
 
 	INSERT INTO Projects (TimeStamp, Tag, TotalCapacity, DevDedication, Remaining, StartDate, TargetDate)
